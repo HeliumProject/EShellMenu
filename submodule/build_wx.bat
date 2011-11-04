5e7a3a0771406869cab011e330e1bebbd266b15a
Geoff Evans <geoff@flummoxed.org>
2011-11-04 16:21:03 -0700

    update build script for wx (don't chdir)

--- submodule/build_wx.bat
+++ submodule/build_wx.bat
@@ -1,7 +1,11 @@
 @echo off
 
+setlocal
+pushd
+
 cd %~dp0\wxWidgets\build\msw
-
 nmake.exe /f makefile.vc BUILD=debug DEBUG_INFO=1
-
 nmake.exe /f makefile.vc BUILD=release DEBUG_INFO=1
+
+endlocal
+popd
