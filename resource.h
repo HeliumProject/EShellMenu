408fdc073f688c01ede82a9d89c9178ead404ca3
Geoff Evans <geoff@flummoxed.org>
2011-12-06 10:57:01 -0800

    3.0.3

--- resource.h
+++ resource.h
@@ -1,6 +1,6 @@
 #define LAUNCHER_VERSION_MAJOR			3
 #define LAUNCHER_VERSION_MINOR			0
-#define LAUNCHER_VERSION_PATCH			2
+#define LAUNCHER_VERSION_PATCH			3
 
 // Next default values for new objects
 // 
