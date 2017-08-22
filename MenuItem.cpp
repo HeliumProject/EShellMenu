47dc8073d93c33ef6330616aed1cdcbb1b15f3ac
Geoff Evans <geoff@flummoxed.org>
2017-08-22 14:31:22 -0700

    don't skip window creation in release builds

--- MenuItem.cpp
+++ MenuItem.cpp
@@ -17,11 +17,7 @@ MenuItem::~MenuItem()
 
 bool MenuItem::Execute()
 {
-#ifdef _DEBUG
 	if ( !EShellMenu::ExecuteCommand( m_Command ) ) 
-#else
-	if ( !EShellMenu::ExecuteCommand( m_Command, false ) ) 
-#endif
 	{
 		tstring error = wxT("Unable to create eshell, with command:\n  ") + m_Command;
 		wxMessageDialog dialog( NULL, error.c_str(), wxT("Error"), wxOK | wxICON_INFORMATION );
